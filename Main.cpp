9b2f1a1e9eb5cd6b421a1cc60caa0f9e2ade9b78
Thales Sabino <tluisrs@gmail.com>
2022-11-05 21:58:24 +0000

    Adding a simple application class

--- Main.cpp
+++ Main.cpp
@@ -9,77 +9,117 @@
 #include <glm/glm.hpp>
 #include <glm/gtx/string_cast.hpp>
 
-constexpr glm::ivec2 WindowSize{ 800, 600 };
-constexpr std::string_view AppName = "\"Simple\" Vulkan Renderer";
+class SimpleVulkanApplication
+{
+public:
+    void Run()
+    {
+        InitWindow();
+        InitVulkan();
+        MainLoop();
+        Shutdown();
+    }
+
+private:
+    void InitWindow()
+    {
+        if (glfwInit() != GLFW_TRUE)
+        {
+            throw std::exception("Error initializing GLFW");
+        }
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        Window = glfwCreateWindow(WindowSize.x, WindowSize.y, AppName.data(), nullptr, nullptr);
+
+        if (Window == nullptr)
+        {
+            throw std::exception("Failed to destroy window");
+        }
+    }
+
+    void InitVulkan()
+    {
+        const std::vector<vk::ExtensionProperties> VulkanInstanceExtentionProperties = vk::enumerateInstanceExtensionProperties();
+        for (const vk::ExtensionProperties& Property : VulkanInstanceExtentionProperties)
+        {
+            std::cout << Property.extensionName << std::endl;
+        }
+        std::cout << std::endl;
+
+        constexpr vk::ApplicationInfo AppInfo
+        {
+            AppName.data(),
+            VK_MAKE_VERSION(0, 0, 0),
+            nullptr,
+            0,
+            VK_API_VERSION_1_1
+        };
+
+        const std::vector<const char*> Layers = { "VK_LAYER_KHRONOS_validation" };
+        const std::vector<const char*> Extensions = {};
+        vk::InstanceCreateInfo InstanceCreateInfo
+        {
+            vk::InstanceCreateFlags{},
+            &AppInfo,
+            Layers,
+            Extensions,
+        };
+
+        VulkanInstance = vk::createInstance(InstanceCreateInfo);
+
+        const std::vector<vk::PhysicalDevice> PhysicalDevices = VulkanInstance.enumeratePhysicalDevices();
+        for (const vk::PhysicalDevice& Device : PhysicalDevices)
+        {
+            vk::PhysicalDeviceProperties DeviceProperties = Device.getProperties();
+
+            std::cout << "Device Name: " << DeviceProperties.deviceName << std::endl;
+            const uint32_t ApiVersion = DeviceProperties.apiVersion;
+            std::cout << "Vulkan Version : " << VK_VERSION_MAJOR(ApiVersion) << "." << VK_VERSION_MINOR(ApiVersion) << "." << VK_VERSION_PATCH(ApiVersion) << std::endl;
+            vk::PhysicalDeviceLimits DeviceLimits = DeviceProperties.limits;
+            std::cout << "Max Compute Shared Memory Size: " << DeviceLimits.maxComputeSharedMemorySize / 1024 << " KB" << std::endl;
+        }
+    }
+
+    void MainLoop()
+    {
+        while (!glfwWindowShouldClose(Window))
+        {
+            glfwPollEvents();
+        }
+    }
+
+    void Shutdown()
+    {
+        VulkanInstance.destroy();
+
+        glfwDestroyWindow(Window);
+
+        glfwTerminate();
+    }
+
+private:
+    constexpr static glm::ivec2 WindowSize{ 800, 600 };
+    constexpr static std::string_view AppName = "\"Simple\" Vulkan Renderer";
+
+    GLFWwindow* Window;
+
+    vk::Instance VulkanInstance;
+};
 
 int main(int ArgC, char* ArgV[])
 {
-    if (glfwInit() != GLFW_TRUE)
+    SimpleVulkanApplication VulkanApp;
+
+    try
     {
-        std::cerr << "Error initializing GLFW" << std::endl;
-        return -1;
+        VulkanApp.Run();
     }
-
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-    GLFWwindow* Window = glfwCreateWindow(WindowSize.x, WindowSize.y, AppName.data(), nullptr, nullptr);
-
-    if (Window == nullptr)
+    catch (const std::exception& Exception)
     {
-        std::cerr << "Failed to destroy window" << std::endl;
-        return -1;
+        std::cerr << Exception.what() << std::endl;
+        return EXIT_FAILURE;
     }
-
-    const std::vector<vk::ExtensionProperties> VulkanInstanceExtentionProperties = vk::enumerateInstanceExtensionProperties();
-    for (const vk::ExtensionProperties& Property : VulkanInstanceExtentionProperties)
-    {
-        std::cout << Property.extensionName << std::endl;
-    }
-    std::cout << std::endl;
-
-    constexpr vk::ApplicationInfo AppInfo
-    {
-        AppName.data(),
-        VK_MAKE_VERSION(0, 0, 0),
-        nullptr,
-        0,
-        VK_API_VERSION_1_1
-    };
-
-    const std::vector<const char*> Layers = { "VK_LAYER_KHRONOS_validation" };
-    const std::vector<const char*> Extensions = {};
-    vk::InstanceCreateInfo InstanceCreateInfo
-    {
-        vk::InstanceCreateFlags{},
-        &AppInfo,
-        Layers,
-        Extensions,
-    };
-
-    vk::Instance VulkanInstance = vk::createInstance(InstanceCreateInfo);
-
-    const std::vector<vk::PhysicalDevice> PhysicalDevices = VulkanInstance.enumeratePhysicalDevices();
-    for (const vk::PhysicalDevice& Device : PhysicalDevices)
-    {
-        vk::PhysicalDeviceProperties DeviceProperties = Device.getProperties();
-
-        std::cout << "Device Name: " << DeviceProperties.deviceName << std::endl;
-        const uint32_t ApiVersion = DeviceProperties.apiVersion;
-        std::cout << "Vulkan Version : " << VK_VERSION_MAJOR(ApiVersion) << "." << VK_VERSION_MINOR(ApiVersion) << "." << VK_VERSION_PATCH(ApiVersion) << std::endl;
-        vk::PhysicalDeviceLimits DeviceLimits = DeviceProperties.limits;
-        std::cout << "Max Compute Shared Memory Size: " << DeviceLimits.maxComputeSharedMemorySize / 1024 << " KB" << std::endl;
-    }
-
-    while (!glfwWindowShouldClose(Window))
-    {
-        glfwPollEvents();
-    }
-
-    VulkanInstance.destroy();
-
-    glfwDestroyWindow(Window);
-
-    glfwTerminate();
 
     return EXIT_SUCCESS;
 }
