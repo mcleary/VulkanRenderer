f8c479872a487ca47a7af74c97ccfd1e3c95e025
Thales Sabino <tluisrs@gmail.com>
2022-11-06 20:39:47 +0000

    Adding a debug utils messenger

--- Main.cpp
+++ Main.cpp
@@ -9,6 +9,22 @@
 #include <glm/glm.hpp>
 #include <glm/gtx/string_cast.hpp>
 
+PFN_vkCreateDebugUtilsMessengerEXT  pfnVkCreateDebugUtilsMessengerEXT;
+PFN_vkDestroyDebugUtilsMessengerEXT pfnVkDestroyDebugUtilsMessengerEXT;
+
+VKAPI_ATTR VkResult VKAPI_CALL vkCreateDebugUtilsMessengerEXT(VkInstance Instance,
+                                                              const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+                                                              const VkAllocationCallbacks* pAllocator,
+                                                              VkDebugUtilsMessengerEXT* pMessenger)
+{
+    return pfnVkCreateDebugUtilsMessengerEXT(Instance, pCreateInfo, pAllocator, pMessenger);
+}
+
+VKAPI_ATTR void VKAPI_CALL vkDestroyDebugUtilsMessengerEXT(VkInstance Instance, VkDebugUtilsMessengerEXT Messenger, VkAllocationCallbacks const* pAllocator)
+{
+    return pfnVkDestroyDebugUtilsMessengerEXT(Instance, Messenger, pAllocator);
+}
+
 class SimpleVulkanApplication
 {
 public:
@@ -38,15 +54,20 @@ private:
         }
     }
 
-    void InitVulkan()
+    std::vector<const char*> GetRequiredVulkanExtensions() const
     {
-        const std::vector<vk::ExtensionProperties> VulkanInstanceExtentionProperties = vk::enumerateInstanceExtensionProperties();
-        for (const vk::ExtensionProperties& Property : VulkanInstanceExtentionProperties)
-        {
-            std::cout << Property.extensionName << std::endl;
-        }
-        std::cout << std::endl;
+        uint32_t GLFWExtensionCount = 0;
+        const char** GLFWExtensions = glfwGetRequiredInstanceExtensions(&GLFWExtensionCount);
 
+        std::vector<const char*> Extensions{ GLFWExtensions, GLFWExtensions + GLFWExtensionCount };
+
+        Extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+
+        return Extensions;
+    }
+
+    void CreateVulkanInstance()
+    {
         constexpr vk::ApplicationInfo AppInfo
         {
             AppName.data(),
@@ -57,8 +78,8 @@ private:
         };
 
         const std::vector<const char*> Layers = { "VK_LAYER_KHRONOS_validation" };
-        const std::vector<const char*> Extensions = {};
-        vk::InstanceCreateInfo InstanceCreateInfo
+        const std::vector<const char*> Extensions = GetRequiredVulkanExtensions();
+        const vk::InstanceCreateInfo InstanceCreateInfo
         {
             vk::InstanceCreateFlags{},
             &AppInfo,
@@ -66,7 +87,39 @@ private:
             Extensions,
         };
 
-        VulkanInstance = vk::createInstance(InstanceCreateInfo);
+        constexpr vk::DebugUtilsMessageSeverityFlagsEXT SeverityFlags = vk::DebugUtilsMessageSeverityFlagBitsEXT::eError | vk::DebugUtilsMessageSeverityFlagBitsEXT::eWarning;
+        constexpr vk::DebugUtilsMessageTypeFlagsEXT MessageTypeFlags = vk::DebugUtilsMessageTypeFlagBitsEXT::eGeneral | vk::DebugUtilsMessageTypeFlagBitsEXT::ePerformance | vk::DebugUtilsMessageTypeFlagBitsEXT::eValidation;
+        const vk::DebugUtilsMessengerCreateInfoEXT DebugUtilsCreateInfo
+        {
+            vk::DebugUtilsMessengerCreateFlagsEXT{},
+            SeverityFlags,
+            MessageTypeFlags,
+            &VulkanDebugCallback,
+            reinterpret_cast<void*>(this)
+        };
+
+        const vk::StructureChain<vk::InstanceCreateInfo, vk::DebugUtilsMessengerCreateInfoEXT> InstanceCreateInfoChain(InstanceCreateInfo, DebugUtilsCreateInfo);
+
+        VulkanInstance = vk::createInstance(InstanceCreateInfoChain.get<vk::InstanceCreateInfo>());
+
+        pfnVkCreateDebugUtilsMessengerEXT = reinterpret_cast<PFN_vkCreateDebugUtilsMessengerEXT>(VulkanInstance.getProcAddr("vkCreateDebugUtilsMessengerEXT"));
+        if (pfnVkCreateDebugUtilsMessengerEXT == nullptr)
+        {
+            throw std::exception("Unable to load PFN_vkCreateDebugUtilsMessengerEXT");
+        }
+
+        pfnVkDestroyDebugUtilsMessengerEXT = reinterpret_cast<PFN_vkDestroyDebugUtilsMessengerEXT>(VulkanInstance.getProcAddr("vkDestroyDebugUtilsMessengerEXT"));
+        if (pfnVkDestroyDebugUtilsMessengerEXT == nullptr)
+        {
+            throw std::exception("Unable to load PFN_vkDestroyDebugUtilsMessengerEXT");
+        }
+
+        VulkanDebugUtilsMessenger = VulkanInstance.createDebugUtilsMessengerEXT(DebugUtilsCreateInfo);
+    }
+
+    void InitVulkan()
+    {
+        CreateVulkanInstance();
 
         const std::vector<vk::PhysicalDevice> PhysicalDevices = VulkanInstance.enumeratePhysicalDevices();
         for (const vk::PhysicalDevice& Device : PhysicalDevices)
@@ -91,11 +144,24 @@ private:
 
     void Shutdown()
     {
+        VulkanInstance.destroyDebugUtilsMessengerEXT(VulkanDebugUtilsMessenger);
         VulkanInstance.destroy();
 
         glfwDestroyWindow(Window);
 
         glfwTerminate();
+    }
+
+    static VKAPI_ATTR VkBool32 VKAPI_CALL VulkanDebugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT MessageSeverity,
+                                                              VkDebugUtilsMessageTypeFlagsEXT MessageType,
+                                                              const VkDebugUtilsMessengerCallbackDataEXT* CallbackData,
+                                                              void* UserData)
+    {
+        const std::string MessageSeverityStr = vk::to_string(static_cast<vk::DebugUtilsMessageSeverityFlagBitsEXT>(MessageSeverity));
+        const std::string MessageTypeStr = vk::to_string(static_cast<vk::DebugUtilsMessageTypeFlagBitsEXT>(MessageType));
+        std::cerr << "[" << MessageSeverityStr << "] " << "[" << MessageTypeStr << "] \n";
+        std::cerr << "\t" << CallbackData->pMessage << "\n" << std::endl;
+        return VK_FALSE;
     }
 
 private:
@@ -104,7 +170,9 @@ private:
 
     GLFWwindow* Window;
 
+    constexpr static bool bEnableVulkanValidationLayers = true;
     vk::Instance VulkanInstance;
+    vk::DebugUtilsMessengerEXT VulkanDebugUtilsMessenger;
 };
 
 int main(int ArgC, char* ArgV[])
@@ -115,9 +183,18 @@ int main(int ArgC, char* ArgV[])
     {
         VulkanApp.Run();
     }
+    catch (const vk::SystemError& Exception)
+    {
+        std::cerr << "vk::SystemError: " << Exception.what() << std::endl;
+    }
     catch (const std::exception& Exception)
     {
-        std::cerr << Exception.what() << std::endl;
+        std::cerr << "std::exception:  " << Exception.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    catch (...)
+    {
+        std::cerr << "Unknown exception" << std::endl;
         return EXIT_FAILURE;
     }
 
