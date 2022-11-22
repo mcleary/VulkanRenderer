97762607c602325d130d21afd8162082f3fceceb
Thales Sabino <tluisrs@gmail.com>
2022-11-22 19:38:16 +0000

    Fix spelling mistakes in Setup.ps1

--- Setup.ps1
+++ Setup.ps1
@@ -1,4 +1,4 @@
 # Setup dependencies using vcpkg
 vcpkg install glm:x64-windows
-vcpkg install glfw:x64-windows
-vkpkg install termcolor:x64-windows
+vcpkg install glfw3:x64-windows
+vcpkg install termcolor:x64-windows
